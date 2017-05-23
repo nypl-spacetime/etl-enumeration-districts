15e2d80b2bfccc8ba4de8b246396a7cef9990e42
Bert Spaan <bert.spaan@gmail.com>
2017-05-23 15:50:15 -0400

    filter incorrect submissions, removed old code

--- enumeration-districts.js
+++ enumeration-districts.js
@@ -7,72 +7,9 @@ const turf = {
   union: require('@turf/union')
 }
 
-// const topojson = require('topojson')
-// const topojsonSimplify = require('topojson-simplify')
-// const topojsonClient = require('topojson-client')
-// const turf = {
-//   midpoint: require('turf-midpoint'),
-//   distance: require('turf-distance')
-// }
-
 const url = 'http://brick-by-brick.herokuapp.com/tasks/trace-maps/submissions/all.ndjson'
 const filename = 'brick-by-brick-submissions.ndjson'
 const year = 1900
-
-// const UNITS = 'meters'
-// const MAX_LINESTRING_DISTANCE = 10
-
-// function geojsonPoint (coordinates) {
-//   return {
-//     type: 'Point',
-//     coordinates
-//   }
-// }
-
-// function geojsonPolygon (coordinates) {
-//   return {
-//     type: 'Polygon',
-//     coordinates: [
-//       coordinates
-//     ]
-//   }
-// }
-
-// function midpoint (a, b) {
-//   return turf.midpoint(geojsonPoint(a), geojsonPoint(b)).geometry.coordinates
-// }
-
-// function addMidpoints(a, b) {
-//   const distance = turf.distance(geojsonPoint(a), geojsonPoint(b), UNITS)
-//   if (distance < MAX_LINESTRING_DISTANCE) {
-//     return [a]
-//   } else {
-//     const m = midpoint(a, b)
-//     return R.unnest([
-//       addMidpoints(a, m),
-//       addMidpoints(m, b)
-//     ])
-//   }
-// }
-
-// function splitLineStrings (coordinate, index, coordinates) {
-//   if (index < coordinates.length - 1) {
-//     return addMidpoints(coordinate, coordinates[index + 1])
-//   } else {
-//     return [coordinate]
-//   }
-// }
-
-// function splitCoordinates (coordinates) {
-//   return R.unnest(coordinates.map(splitLineStrings))
-// }
-
-// function splitFeatureCoordinates (feature) {
-//   var coordinates = feature.geometry.coordinates[0]
-//   return Object.assign(feature, {
-//     geometry: geojsonPolygon(splitCoordinates(coordinates))
-//   })
-// }
 
 function featuresFromGroup (obj) {
   return R.toPairs(obj)
@@ -101,6 +38,8 @@ function transform (config, dirs, tools, callback) {
     .split()
     .compact()
     .map(JSON.parse)
+    // TODO: log if submissions without data or geojson are encountered!
+    .filter((submission) => submission && submission.data && submission.data.geojson)
     .map((submission) => submission.data.geojson.features)
     .flatten()
     .compact()
